66a45c7487086d2fcdf33a0fc6cc37de75af3668
Thiago Melo <4thiagomelo5@gmail.com>
2020-08-15 01:03:58 -0300

    commit init

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import YoutubePlayer from 'react-native-youtube-iframe'
 
 export default function App() {
+  const playerRef = useRef()
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Inserindo Iframe do Youtube no app React Native </Text>
+      <YoutubePlayer 
+        videoId={'RpDkRX0cDAY'}
+        play={true}
+        height={400} 
+        width={400} 
+        ref={playerRef}
+        volume={50}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +27,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
